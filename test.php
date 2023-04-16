b271847799f428100aaf9eda5ae334e6e26cf75e
SuiCxDe <nitroxcyanide@gmail.com>
2023-04-16 23:49:34 +0530

    update modal styles + fix mobile issues

--- test.php
+++ test.php
@@ -10,37 +10,44 @@ License: GPL2
 */
 // Add the product boxes with logo and price
 require_once ABSPATH . '/wp-admin/includes/image.php';
-function product_price_display() {
+function product_price_display()
+{
     global $product;
     $logos = array(
-        'logo' => get_option('logo'),
-        'logo2' => get_option('logo2'),
-        'logo3' => get_option('logo3'),
-        'logo4' => get_option('logo4'),
-        'logo5' => get_option('logo5'),
+        'logo' => get_option('logo') ,
+        'logo2' => get_option('logo2') ,
+        'logo3' => get_option('logo3') ,
+        'logo4' => get_option('logo4') ,
+        'logo5' => get_option('logo5') ,
     );
 
-    if ( $product->get_price() ) {
-        $price = wc_price( $product->get_price() );
+    if ($product->get_price())
+    {
+        $price = wc_price($product->get_price());
         $keys = array_keys($logos);
         echo '<div class="product-row">';
-        foreach ($keys as $index => $key) {
+        foreach ($keys as $index => $key)
+        {
             $logo = $logos[$key];
             $discount = get_option($key . '_discount', 0);
-            $lastPrice = $product->get_price() * (1 - ($discount/100));
-            if (!empty($logo)) {
-                $logoImg = '<img src="' . esc_url($logo) . '" width="100" alt="Logo ' . ($index+1) . '">';
-                echo '<div class="product-box" data-discount="' . esc_attr($discount) . '">' . $logoImg . '<span class="product-price">' .  wc_price($lastPrice) . '</span></div>';
+            $name = get_option($key . '_payment_name', 0);
+            $description = get_option($key . '_description', 0);
+            $lastPrice = $product->get_price() * (1 - ($discount / 100));
+            if (!empty($logo))
+            {
+                $logoImg = '<img src="' . esc_url($logo) . '" width="100" alt="Logo ' . ($index + 1) . '">';
+                echo '<div class="product-box" data-discount="' . esc_attr($discount) . '" data-name="' . esc_attr($name) . '" data-description="' . esc_attr($description) . '">' . $logoImg . '<span class="product-price">' . wc_price($lastPrice) . '</span></div>';
             }
         }
         echo '</div>';
     }
 }
 
-add_action( 'woocommerce_single_product_summary', 'product_price_display', 20 );
+add_action('woocommerce_single_product_summary', 'product_price_display', 20);
 
 // Add styles for the product boxes
-function product_box_styles() {
+function product_box_styles()
+{
     echo '<style>
         .product-row {
             display: flex;
@@ -68,37 +75,84 @@ function product_box_styles() {
         .product-row .product-box:first-child:last-child {
             width: 100%;
         }
-        .modal {
+.modal {
             display: none;
             position: fixed;
-            z-index: 1;
+            z-index: 99999;
             left: 0;
             top: 0;
             width: 100%;
             height: 100%;
-            overflow: auto;
-            background-color: rgba(0, 0, 0, 0.4);
+            background-color: rgba(0, 0, 0, 0.6);
         }
-  .modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-  max-width: 500px;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
-      .close { color: #aaa; position: absolute; top: 5px; right: 10px; font-size: 28px; font-weight: bold; }
+        .modal-header {
+            display: flex;
+            align-items: left;
+            justify-content: space-between;
+            background-color: #fff;
+              font-size: 16px;
+            border-bottom: 1px solid #ccc;
+            padding:0;
+            margin-bottom:5px;
+        }
+        .modal-header h5 {
+            font-weight: bold;
 
-        .close:hover,
-        .close:focus {
-            color: black;
-            text-decoration: none;
-            cursor: pointer;
         }
+
+            .modal p {
+                font-size:16px
+            }
+
+
+        .modal-content {
+            background-color: #fff;
+            padding: 20px;
+            padding-bottom:5px;
+            border-radius: 5px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            max-width: 500px;
+            width: 30%;
+            max-height: 80%;
+            overflow-y: auto;
+        }
+        .modal-header .close {
+    color: #8B0000;
+    font-size: 30px;
+    padding:0;
+              padding-top: 10px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: color 0.3s ease-in-out;
+    margin-right: 5px;
+}
+        .close {
+            color: #8B0000;
+            font-size: 30px;
+
+            font-weight: bold;
+            cursor: pointer;
+            transition: color 0.3s ease-in-out;
+        }
+        .close:hover {
+            color: #000;
+        }
+.discount-circle {
+  display: inline-block;
+  height: 30px;
+  border-radius: 15px;
+  background-color: #ff0000;
+  color: #fff;
+  text-align: center;
+  line-height: 30px;
+  font-weight: bold;
+  font-size: 16px;
+  margin-left: 10px;
+  padding: 0 10px;
+}
     /* Media query for mobile devices */
     @media only screen and (max-width: 768px) {
         .modal-content {
@@ -120,7 +174,9 @@ function product_box_styles() {
         jQuery(document).ready(function() {
             jQuery(".product-box").click(function() {
                 var discount = jQuery(this).data("discount");
-                var modal = "<div class=\"modal\"><div class=\"modal-content\"><span class=\"close\">&times;</span><p>This is your discount:</p><p>" + discount + "%</p></div></div>";
+                var name = jQuery(this).data("name");
+                var description = jQuery(this).data("description");
+                var modal = "<div class=\"modal\"><div class=\"modal-content\"><div class=\"modal-header\"><h5>" + name + "</h5><span class=\"close\">&times;</span></div><p>" + description + "</p><p>Discount Percentage: <span class=\"discount-circle\">" + discount + "%</span></p></div></div>";
                 jQuery("body").append(modal);
                 jQuery(".modal").show();
                 jQuery(".close").click(function() {
@@ -134,52 +190,62 @@ function product_box_styles() {
     </script>';
 }
 
-
 add_action('wp_head', 'product_box_styles');
 
 // Adjust margin top of short description
-function adjust_short_desc_margin_top() {
+function adjust_short_desc_margin_top()
+{
     echo '<style>.woocommerce-product-details__short-description { margin-top: 7px; }</style>';
 }
-add_action( 'wp_head', 'adjust_short_desc_margin_top' );
+add_action('wp_head', 'adjust_short_desc_margin_top');
 
-function wpdocs_register_custom_settings_page() {
-    add_options_page(
-        'Custom Settings', // Page Title
-        'Custom Settings', // Menu Title
-        'manage_options', // Capability
-        'custom_settings', // Menu Slug
-        'custom_settings_page' // Callback function to render the page
+function wpdocs_register_custom_settings_page()
+{
+    add_options_page('Custom Settings', // Page Title
+    'Custom Settings', // Menu Title
+    'manage_options', // Capability
+    'custom_settings', // Menu Slug
+    'custom_settings_page'
+    // Callback function to render the page
     );
 }
 add_action('admin_menu', 'wpdocs_register_custom_settings_page');
 // Register a new setting in the WordPress database
 // Register a new setting in the WordPress database
-function register_custom_settings() {
-    register_setting(
-        'custom_settings_group', // Option group
-        'logoOne', // Option name
-        array(
-            'sanitize_callback' => 'esc_url_raw' // Sanitize the uploaded image URL
-        )
-    );
+function register_custom_settings()
+{
+    register_setting('custom_settings_group', // Option group
+    'logoOne', // Option name
+    array(
+        'sanitize_callback' => 'esc_url_raw'
+        // Sanitize the uploaded image URL
+
+    ));
 }
 
 add_action('admin_init', 'register_custom_settings');
 
 // Callback function to render the custom settings page
-function custom_settings_page() {
-    ?>
+function custom_settings_page()
+{
+?>
     <div class="wrap">
         <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
         <form method="post" action="options.php" enctype="multipart/form-data">
             <?php
-                settings_fields('custom_settings_group');
-                do_settings_sections('custom_settings');
-            ?>
+    settings_fields('custom_settings_group');
+    do_settings_sections('custom_settings');
+?>
             <table class="form-table">
-                <?php $logos = array( 'logo', 'logo2', 'logo3', 'logo4','logo5'); ?>
-                <?php foreach ($logos as $logo) { ?>
+                <?php $logos = array(
+        'logo',
+        'logo2',
+        'logo3',
+        'logo4',
+        'logo5'
+    ); ?>
+                <?php foreach ($logos as $logo)
+    { ?>
                     <tr>
                         <th scope="row">
                             <label for="<?php echo esc_attr($logo); ?>"><?php echo esc_html($logo); ?></label>
@@ -187,18 +253,32 @@ function custom_settings_page() {
                         <td>
                             <?php $logo_url = get_option($logo); ?>
                             <input type="hidden" name="<?php echo esc_attr($logo); ?>" value="<?php echo esc_attr($logo_url); ?>" />
-                            <?php if ($logo_url) { ?>
+                            <?php if ($logo_url)
+        { ?>
                                 <img src="<?php echo esc_url($logo_url); ?>" alt="<?php echo esc_attr($logo); ?>" /><br />
                                 <button type="button" class="delete-logo" data-logo="<?php echo esc_attr($logo); ?>"><?php _e('Delete', 'textdomain'); ?></button>
-                            <?php } ?>
+                            <?php
+        } ?>
                             <input type="file" name="<?php echo esc_attr($logo); ?>_file" id="<?php echo esc_attr($logo); ?>_file" />
                             <br>
-                            <label for="<?php echo esc_attr($logo); ?>_discount">Discount Percentage</label>
+                            <label for="<?php echo esc_attr($logo); ?>_discount">Discount Percentage</label><br>
                             <input type="number" min="0" max="100" step="0.1" name="<?php echo esc_attr($logo); ?>_discount" id="<?php echo esc_attr($logo); ?>_discount" value="<?php echo esc_attr(get_option($logo . '_discount', 0)); ?>" />
+
+                            <br>
+                            <label for="<?php echo esc_attr($logo); ?>_payment_name">Payment Method Name</label>
+                            <input type="text" name="<?php echo esc_attr($logo); ?>_payment_name" id="<?php echo esc_attr($logo); ?>_payment_name" value="<?php echo esc_attr(get_option($logo . '_payment_name', '')); ?>" />
+                            <br>
+                            <label for="<?php echo esc_attr($logo); ?>_description">Description</label>
+                            <input type="text" name="<?php echo esc_attr($logo); ?>_description" id="<?php echo esc_attr($logo); ?>_description" value="<?php echo esc_attr(get_option($logo . '_description', '')); ?>" />
+                            <br>
+
+
+
                         </td>
                     </tr>
 
-                <?php } ?>
+                <?php
+    } ?>
             </table>
     <script>
     jQuery(document).ready(function($) {
@@ -233,11 +313,20 @@ function custom_settings_page() {
 }
 
 // Handle uploaded image and update the 'logoOne' option in the database
-function handle_logo_uploads() {
-    $logos = array('logo', 'logo2', 'logo3', 'logo4','logo5');
-    foreach ($logos as $logo) {
+function handle_logo_uploads()
+{
+    $logos = array(
+        'logo',
+        'logo2',
+        'logo3',
+        'logo4',
+        'logo5'
+    );
+    foreach ($logos as $logo)
+    {
         $logo_url = get_option($logo);
-        if (!empty($_FILES[$logo . '_file']['name'])) {
+        if (!empty($_FILES[$logo . '_file']['name']))
+        {
             $file = $_FILES[$logo . '_file'];
             $upload_dir = wp_upload_dir();
             $upload_path = $upload_dir['path'] . '/';
@@ -246,36 +335,54 @@ function handle_logo_uploads() {
             move_uploaded_file($file['tmp_name'], $file_path);
             $logo_url = $upload_dir['url'] . '/' . $file_name;
             update_option($logo, $logo_url);
-            error_log('This is a debug message');
-            error_log('This is a debug message');
         }
 
         // Update logo discount
-        if (isset($_POST[$logo . '_discount'])) {
-$discount = floatval($_POST[$logo . '_discount']);
+        if (isset($_POST[$logo . '_discount']))
+        {
+            $discount = floatval($_POST[$logo . '_discount']);
 
             update_option($logo . '_discount', $discount);
+        }
+
+        // Update payment method name
+        if (isset($_POST[$logo . '_payment_name']))
+        {
+            $payment_name = $_POST[$logo . '_payment_name'];
+
+            update_option($logo . '_payment_name', $payment_name);
+        }
+
+        // Update payment method description
+        if (isset($_POST[$logo . '_description']))
+        {
+            $payment_method_description = sanitize_textarea_field($_POST[$logo . '_description']);
+
+            update_option($logo . '_description', $payment_method_description);
         }
     }
 }
 
-
-
 add_action('admin_init', 'handle_logo_uploads');
-
 
 add_action('wp_ajax_delete_logo', 'my_delete_logo_function');
 
-function my_delete_logo_function() {
+function my_delete_logo_function()
+{
     $logo = $_POST['logo'];
     delete_option($logo);
-delete_option($logo . '_discount');
-$logo_url = get_option($logo);
-$upload_dir = wp_upload_dir();
-$file_path = str_replace($upload_dir['url'], $upload_dir['path'], $logo_url);
-  $file_path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $file_path);
-if (file_exists($file_path)) {
-    unlink($file_path);
-}
+    delete_option($logo . '_discount');
+    $logo_url = get_option($logo);
+    $upload_dir = wp_upload_dir();
+    $file_path = str_replace($upload_dir['url'], $upload_dir['path'], $logo_url);
+    $file_path = str_replace(array(
+        '/',
+        '\\'
+    ) , DIRECTORY_SEPARATOR, $file_path);
+    if (file_exists($file_path))
+    {
+        unlink($file_path);
+    }
     wp_die();
 }
+
